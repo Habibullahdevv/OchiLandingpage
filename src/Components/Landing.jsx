7da81d6d8fa196fb2fb818be49d9bbb5f6707478
Habibullahdevv <habibullahmushtaq2005@gmail.com>
2024-11-26 14:17:23 +0500

    added

---
+++ src/Components/Landing.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Landing = () => {
+  return (
+    <div className='w-full h-screen bg-black'>
+      <div className="textstructure">
+        <div className="masker font-['Founders Frotesk']">
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Landing
